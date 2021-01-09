759b9fac294d8df0e3059953aaafd1eb3e20da59
Ruben Rodriguez <rrodriguez6@outlook.com>
2021-01-09 13:05:34 -0600

    added download form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import DownloadForm from "../components/download-form"
 
 export default function Home() {
   return (
     <Layout>
       <p>
-        Enter the the URL of a YouTube video below and click "Download" to
-        download it as an mp3!
+        Steps to download a song
       </p>
+      <ul>
+        <li>Find a song you like on YouTube</li>
+        <li>Copy the URL</li>
+        <li>Paste the URL in the text box (right click works!)</li>
+        <li>Click download</li>
+        <li>Wait for it to finish converting to an mp3</li>
+        <li>You should get a popup saying if you want to save it. If not, it's in your Downloads folder</li>
+        <li>If you click on the mp3 file, it should automatically add to iTunes</li>
+        <li>Enjoy! Love, Junior</li>
+      </ul>
+      <DownloadForm />
     </Layout>
   )
 }
